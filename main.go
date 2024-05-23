ba1593bec82d79c781d346feb85a35a948eab0dc
jahsimelvin <155954108+jahsimelvin@users.noreply.github.com>
2024-05-23 12:34:20 +0200

    push my new data base because the first data base is hard

--- main.go
+++ main.go
@@ -12,16 +12,25 @@ func newsHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./html/News.html")
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./html/form.html")
+}
+
+func form2Handler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./html/form2².html")
+}
+
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("bjr"))
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", formHandler)
 	http.Handle("/css/", http.FileServer(http.Dir(".")))
 	http.Handle("/pictures/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/news", newsHandler)
 	http.HandleFunc("/post", postHandler)
+
 
 	http.ListenAndServe(":8080", nil)
 }
